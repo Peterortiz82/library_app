396bd1071199b69e8e30fd29a45480f9b3732dc3
Peter Ortiz <peterortiz@Peter-Ortiz-MBP-739.local>
2016-09-16 12:13:42 -0400

    added some checks in the index controller

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+  isDisabled: true,
+
+  emailAddress: '',
+
+  isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+  isDisabled: Ember.computed.not('isValid')
+
 });
